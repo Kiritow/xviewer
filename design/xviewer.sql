0b25618f0fccc071556826b6c9994eebe7b47004
Kiritow <1362050620@qq.com>
2020-08-17 17:13:16 +0800

    Add DB Design

---
+++ design/xviewer.sql
@@ -0,0 +1,118 @@
+-- MySQL dump 10.16  Distrib 10.1.44-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: 127.1    Database: xviewer
+-- ------------------------------------------------------
+-- Server version	10.4.8-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `covers`
+--
+
+DROP TABLE IF EXISTS `covers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `covers` (
+  `id` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `covers_ibfk_1` FOREIGN KEY (`id`) REFERENCES `objects` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `objects`
+--
+
+DROP TABLE IF EXISTS `objects`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `objects` (
+  `id` varchar(255) NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  `mtime` int(11) DEFAULT NULL,
+  `fsize` int(11) DEFAULT NULL,
+  `createtime` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updatetime` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Temporary table structure for view `videocheck`
+--
+
+DROP TABLE IF EXISTS `videocheck`;
+/*!50001 DROP VIEW IF EXISTS `videocheck`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE TABLE `videocheck` (
+  `id` tinyint NOT NULL,
+  `cid` tinyint NOT NULL,
+  `filename` tinyint NOT NULL,
+  `watchcount` tinyint NOT NULL
+) ENGINE=MyISAM */;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `videos`
+--
+
+DROP TABLE IF EXISTS `videos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `videos` (
+  `id` varchar(255) NOT NULL,
+  `coverid` varchar(255) DEFAULT NULL,
+  `videotime` int(11) DEFAULT NULL,
+  `watchcount` int(11) DEFAULT NULL,
+  `watchtime` int(11) DEFAULT NULL,
+  `uploader` varchar(255) DEFAULT NULL,
+  `tags` varchar(255) DEFAULT NULL,
+  `createtime` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updatetime` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`),
+  KEY `coverid` (`coverid`),
+  CONSTRAINT `videos_ibfk_1` FOREIGN KEY (`id`) REFERENCES `objects` (`id`),
+  CONSTRAINT `videos_ibfk_2` FOREIGN KEY (`coverid`) REFERENCES `objects` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Final view structure for view `videocheck`
+--
+
+/*!50001 DROP TABLE IF EXISTS `videocheck`*/;
+/*!50001 DROP VIEW IF EXISTS `videocheck`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8 */;
+/*!50001 SET character_set_results     = utf8 */;
+/*!50001 SET collation_connection      = utf8_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `videocheck` AS select `videos`.`id` AS `id`,`videos`.`coverid` AS `cid`,`objects`.`filename` AS `filename`,`videos`.`watchcount` AS `watchcount` from (`videos` join `objects` on(`videos`.`id` = `objects`.`id`)) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-08-17 16:55:50
