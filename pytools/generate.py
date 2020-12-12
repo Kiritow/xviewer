059b6162b5066866ef53ccf3cbe94276c968b23e
Kiritow <1362050620@qq.com>
2020-12-13 00:26:57 +0800

    Update generate tools

--- pytools/generate.py
+++ pytools/generate.py
@@ -1,31 +1,14 @@
 # -*- coding: utf-8 -*-
-
-from UniTools.UniCon import UniCon
-from multiprocessing import Process
 import os
 import hashlib
 import subprocess
 import uuid
+import time
 import json
+from UniTools.UniCon import UniCon
 
 
-CONFIG_DIR = "/mnt/d/xviewer/config"
-
-# --- Initialize with Config ---
-with open(os.path.join(CONFIG_DIR, "settings.json")) as f:
-    j = json.loads(f.read())
-    ROOT_DIR = j["rootdir"]
-
-with open(os.path.join(CONFIG_DIR, "mysql_config.json")) as f:
-    j = json.loads(f.read())
-    DB_HOST = j["host"]
-    DB_USER = j["user"]
-    DB_PASS = j["password"]
-    DB_NAME = j["database"]
-    DB_PORT = j.get("port", 3306)
-
-
-def GetFileHash(filepath):
+def get_file_hash(filepath):
     with open(filepath) as f:
         content = f.read()
     sha = hashlib.sha256()
@@ -33,18 +16,21 @@ def GetFileHash(filepath):
     return sha.hexdigest()
 
 
-def GenerateCover(video_path):
-    cover_path = "../temp/{}.png".format(uuid.uuid4())
+def generate_cover(root_dir, video_path):
+    cover_path = os.path.join(root_dir, "temp", "{}.png".format(uuid.uuid4()))
     print "Generating cover for {} to {}...".format(video_path, cover_path)
     subprocess.check_call(["ffmpeg", "-ss", "00:00:05.000", "-i", video_path, "-vframes", "1", cover_path, "-y"])
-    return cover_path, GetFileHash(cover_path)
+    return cover_path, get_file_hash(cover_path)
 
 
 if __name__ == "__main__":
-    os.chdir(ROOT_DIR)
+    with open("config.json") as f:
+        config = json.loads(f.read())
+
+    root_dir = config["root"]
 
     tasks = []
-    for root, dirs, files in os.walk("./objects"):
+    for root, dirs, files in os.walk(os.path.join(root_dir, "objects")):
         for name in files:
             fullpath = os.path.join(root, name)
             if "." in name:
@@ -53,16 +39,23 @@ if __name__ == "__main__":
         for name in dirs:
             print os.path.join(root, name)
 
-    conn = UniCon.connect_mysql(DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME)
+    if not tasks:
+        print "no task found, now exit."
+        exit(0)
+
+    print "{} tasks found".format(len(tasks))
+
+    conn = UniCon.connect_mysql(config["host"], config["port"], config["username"], config["password"], config["database"])
     result = conn.query("select * from objects")
     idset = {row['id']: row['filename'] for row in result}
     for fullpath, filename in tasks:
-        video_hash = GetFileHash(fullpath)
+        print "Computing hash of file: {}".format(fullpath)
+        video_hash = get_file_hash(fullpath)
         if video_hash in idset:
             print "[Skipped] Video already exists: {}\n  Record name: {}\n  hash: {}".format(filename, idset[video_hash], video_hash)
             continue
-        
-        cover_path, cover_hash = GenerateCover(fullpath)
+
+        cover_path, cover_hash = generate_cover(root_dir, fullpath)
         if cover_hash in idset:
             print "[Ignored] Cover {} already exists."
             cover_new = False
@@ -71,12 +64,12 @@ if __name__ == "__main__":
 
         print "Reading video file info..."
         video_info = os.stat(fullpath)
-        video_modify_time = int(video_info.st_mtime)
+        video_modify_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(video_info.st_mtime))
         video_size = video_info.st_size
 
         print "Reading cover file info..."
         cover_info = os.stat(cover_path)
-        cover_modify_time = int(cover_info.st_mtime)
+        cover_modify_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(cover_info.st_mtime))
         cover_size = cover_info.st_size
 
         print "Creating video objects in db..."
@@ -89,14 +82,15 @@ if __name__ == "__main__":
         conn.execute("insert into videos(id, coverid) values (%s, %s)", [video_hash, cover_hash])
 
         print "Renaming video object: {} -> {}".format(filename, video_hash)
-        os.rename(fullpath, "./objects/{}".format(video_hash))
+        os.rename(fullpath, os.path.join(root_dir, "objects", video_hash))
         if cover_new:
             print "Moving cover object: {}".format(cover_hash)
-            os.rename(cover_path, "./objects/{}".format(cover_hash))
-        
+            os.rename(cover_path, os.path.join(root_dir, "objects", cover_hash))
+
         print "Done. New video added: {}".format(filename)
         conn.commit()
-        
+
+        # This might be slow, but it is simple.
         print "Reloading idset..."
         result = conn.query("select * from objects")
         idset = {row['id']: row['filename'] for row in result}
