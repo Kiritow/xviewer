62ab0bc7af208ea5d7c02cc7cec390d0cd842c4f
Kiritow <1362050620@qq.com>
2021-12-30 15:52:49 +0000

    fix backup script

--- pytools/create_backup.sh
+++ pytools/create_backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mysqldump -h$DB_HOST -P$DB_PORT -u$DB_USER $p$DB_PASS --single-transaction $DB_NAME > "backup_$(date +"%Y%m%d_%I%M%S").sql"
+mysqldump -h$DB_HOST -P$DB_PORT -u$DB_USER -p$DB_PASS --column-statistics=0 --single-transaction $DB_NAME | gzip > "backup_$(date +"%Y%m%d_%I%M%S").sql"
